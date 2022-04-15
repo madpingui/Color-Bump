bb2c99332b700c83429380760ffbc7b370ee0b5a
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-15 18:07:51 -0500

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float thrust = 150f;
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float wallDistance = 5f;
+    [SerializeField]
+    private float minCamDistance = 3f;
+
+    private Vector2 lastMousePos;
+
+    void Update()
+    {
+        if (GameManager.singleton.GameEnded)
+            return;
+
+        Vector2 deltaPos = Vector2.zero;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (!GameManager.singleton.GameStarted)
+                GameManager.singleton.StartGame();
+
+
+            Vector2 currentMousePos = Input.mousePosition;
+
+            if(lastMousePos == Vector2.zero)
+                lastMousePos = currentMousePos;
+
+            deltaPos = currentMousePos - lastMousePos;
+            lastMousePos = currentMousePos;
+
+            Vector3 force = new Vector3(deltaPos.x, 0, deltaPos.y) * thrust;
+            rb.AddForce(force);
+        }
+        else
+        {
+            lastMousePos = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if (GameManager.singleton.GameEnded)
+        //    return;
+
+        if (GameManager.singleton.GameStarted)
+        {
+            rb.MovePosition(transform.position + Vector3.forward * 15 * Time.fixedDeltaTime);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if(transform.position.x < -wallDistance)
+        {
+            pos.x = -wallDistance;
+        }
+        else if(transform.position.x > wallDistance)
+        {
+            pos.x = wallDistance;
+        }
+
+        if(transform.position.z < Camera.main.transform.position.z + minCamDistance)
+        {
+            pos.z = Camera.main.transform.position.z + minCamDistance;
+        }
+
+        transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (GameManager.singleton.GameEnded)
+            return;
+
+        if(collision.gameObject.tag == "Death")
+            GameManager.singleton.EndGame(false);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager singleton;
+    public bool GameStarted { get; private set; }
+    public bool GameEnded { get; private set; }
+
+    [SerializeField]private float slowMotionFactor = 0.1f;
+    [SerializeField] private Transform startTransform;
+    [SerializeField] private Transform goalTransform;
+    [SerializeField] private BallController ball;
+
+    public float EntireDistance { get; private set; }
+    public float DistanceLeft { get; private set; }
+
+    private void Start()
+    {
+        EntireDistance = goalTransform.position.z - startTransform.position.z;
+    }
+
+
+    private void Awake()
+    {
+        if(singleton == null)
+        {
+            singleton = this;
+        }
+        else if(singleton != this)
+        {
+            Destroy(gameObject);
+        }
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
+    }
+
+    public void StartGame()
+    {
+        GameStarted = true;
+    }
+
+    public void EndGame(bool win)
+    {
+        GameEnded = true;
+
+        if (!win)
+        {
+            Invoke("RestartGame", 2f * slowMotionFactor);
+            Time.timeScale = slowMotionFactor;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+        else
+        {
+            Invoke("RestartGame", 2f);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Update()
+    {
+        DistanceLeft = Vector3.Distance(ball.transform.position, goalTransform.position);
+
+        if (DistanceLeft > EntireDistance)
+            DistanceLeft = EntireDistance;
+
+        if (ball.transform.position.z > goalTransform.transform.position.z)
+            DistanceLeft = 0;
+    }
+}
